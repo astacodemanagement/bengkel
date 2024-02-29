7a66012150d49def0b6707881a46e5a4afd6cde2
putradongkal <qcutterz@gmail>
2024-02-29 17:01:34 +0700

    Fix setting: add image

--- application/views/setting.php
+++ application/views/setting.php
@@ -1,4 +1,3 @@
-        
         <div class="breadcrumbs">
             <div class="col-sm-4">
                 <div class="page-header float-left">
@@ -11,7 +10,7 @@
                 <div class="page-header float-right">
                     <div class="page-title">
                         <ol class="breadcrumb text-right">
-                            <li><a href="<?=base_url("dashboard");?>">Dashboard</a></li>
+                            <li><a href="<?= base_url("dashboard"); ?>">Dashboard</a></li>
                             <li class="active">Pengaturan</li>
                         </ol>
                     </div>
@@ -25,15 +24,15 @@
                         <div class="col-md-6 col-sm-12">
                             <div class="form-group">
                                 <label>Logo Bengkel</label>
-                                <input type="file" class="dropify" data-default-file="<?=base_url("img/logo.png");?>" />
+                                <input type="file" class="dropify" data-default-file="<?= $this->shop_info->get_image_url(); ?>" />
                             </div>
                             <div class="form-group">
                                 <label>Nama Bengkel</label>
-                                <input type="text" name="name" class="form-control" value="<?=$this->shop_info->get_shop_name();?>">
+                                <input type="text" name="name" class="form-control" value="<?= $this->shop_info->get_shop_name(); ?>">
                             </div>
                             <div class="form-group">
                                 <label>Alamat</label>
-                                <textarea name="address" class="form-control"><?=$this->shop_info->get_shop_address();?></textarea>
+                                <textarea name="address" class="form-control"><?= $this->shop_info->get_shop_address(); ?></textarea>
                             </div>
                         </div>
                     </div>
@@ -44,34 +43,35 @@
         </div>
 
         <script>
-            $(".btn-save").on("click",function(){
-                    var form = new FormData();
-                    form.append("name",jQuery('input[name=name]').val());
-                    form.append("address",jQuery('textarea[name=address]').val());
-                    form.append("userfile",jQuery('.dropify')[0].files[0]);
-                    jQuery.ajax({
-                        url: "<?=base_url("setting/save_info");?>",
-                        method: "POST",
-                        data: form,
-                        dataType: "json",
-                        processData: false,
-                        contentType: false,
-                        success: function(data){
-                            if(data.status) {
-                                Swal.fire(
-                                    'Berhasil',
-                                    data.msg,
-                                    'success'
-                                )
-                            } else {
-                                Swal.fire(
-                                    'Gagal',
-                                    data.msg,
-                                    'error'
-                                )
-                            }
+            $(".btn-save").on("click", function() {
+                var form = new FormData();
+                form.append("name", jQuery('input[name=name]').val());
+                form.append("address", jQuery('textarea[name=address]').val());
+                form.append("image", jQuery('.dropify')[0].files[0]);
+
+                jQuery.ajax({
+                    url: "<?= base_url("setting/save_info"); ?>",
+                    method: "POST",
+                    data: form,
+                    dataType: "json",
+                    processData: false,
+                    contentType: false,
+                    success: function(data) {
+                        if (data.status) {
+                            Swal.fire(
+                                'Berhasil',
+                                data.msg,
+                                'success'
+                            )
+                        } else {
+                            Swal.fire(
+                                'Gagal',
+                                data.msg,
+                                'error'
+                            )
                         }
-                    });
+                    }
+                });
             })
 
             $('.dropify').dropify();
